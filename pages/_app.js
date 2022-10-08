02d57f6892bf0179d38c9c9e3012a9899b3ada9e
0xSimbaCrypto <0xSimbaCrypto@proton.me>
2022-10-08 12:33:36 +0800

    Added Backend and Contract Integration

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,57 @@
 import '../styles/globals.css'
-import { ThirdwebWeb3Provider } from "@3rdweb/hooks";
-//import type { AppProps } from 'next/app'
-
-const supportedChainIds = [4]
-const connectors = {
-  injected: {},
-}
+import { Contract, ethers } from 'ethers'
+import { useState, useEffect } from 'react'
+import MarketplaceABI from "../contractsABI/Marketplace.json"
+import MintABI from "../contractsABI/Mint.json"
+import Header from "../components/Header"
+import { marketplaceAddress, mintAddress } from '../config'
 
 function MyApp({ Component, pageProps }) {
+  const [marketplace, setMarketplace] = useState({})
+  const [nft, setNFT] = useState({})
+  const [walletAccount, setWalletAccount] = useState(null)
+  const [walletStatus, setWalletStatus] = useState(false)
+
+
+  useEffect(() => {
+
+  }, [])
+
+  // Provider and Signer
+  const handler = async () => {
+    // Check if MetaMask is installed.
+    if (window.ethereum) {
+
+        try {
+            const accounts = await window.ethereum.request({ method: 'eth_requestAccounts'});
+            setWalletAccount(accounts[0])
+        } catch (error) {
+            console.log("Error", error)
+        }
+
+    } else {
+        alert("Please install MetaMask")
+    }
+    
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const signer = provider.getSigner()
+    
+    loadContracts(signer)
+
+  }
+
+
+  const loadContracts = async (signer) => {
+    const marketplace =  new ethers.Contract(marketplaceAddress, MarketplaceABI.abi, signer);
+    setMarketplace(marketplace)
+    const nft =  new ethers.Contract(mintAddress, MintABI.abi, signer)
+    setNFT(nft)
+
+  };
+
+
   return (
-    <ThirdwebWeb3Provider supportedChainIds={supportedChainIds} connectors={connectors}>
-      <Component {...pageProps} />
-    </ThirdwebWeb3Provider>
+      <Component {...pageProps} handler={handler} walletAccount={walletAccount} nft={nft} marketplace={marketplace} />
   )
 }
 
