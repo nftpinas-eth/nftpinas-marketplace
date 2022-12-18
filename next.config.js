3daadfc9f729f2ac90f0201160f4146fa3e07924
0xSimbaCrypto <0xSimbaCrypto@proton.me>
2022-12-18 10:49:03 +0800

    Add Environment Package and Variables for Production

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
+require("dotenv").config()
+
 module.exports = {
   reactStrictMode: true,
   images: {
     domains: ['nftpinas.infura-ipfs.io']
+  },
+  env: {
+    PROJECTID: process.env.PROJECTID,
+    PROJECTSECRET: process.env.PROJECTSECRET,
+    API_URL: process.env.API_URL
   }
 }
