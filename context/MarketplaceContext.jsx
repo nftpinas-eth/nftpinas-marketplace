33ef6f95aab5aea05431b092bd9638c88ea01b16
0xSimbaCrypto <0xSimbaCrypto@proton.me>
2023-01-12 16:00:36 +0800

    Update/Add Multiple Function to Fetch and Update Data from Blockchain to API System

--- context/MarketplaceContext.jsx
+++ context/MarketplaceContext.jsx
@@ -1,13 +1,16 @@
 import React,  {useContext, useEffect, useState } from 'react'
 import { ethers } from 'ethers'
 import Router from 'next/router'
+import { Provider } from "zksync-web3"
+
 
 // Contract Address and ABI Code Import
 import { marketplaceAddress } from '../config'
 import MarketplaceABI from "../contractsABI/Marketplace.json"
 
 // Infura Client Import
-import { client } from "../lib/infura_client";
+import { client } from "../lib/infura_client"
+import axios from 'axios'
 
 
 
@@ -16,7 +19,12 @@ export const MarketplaceContext = React.createContext()
 export const MarketplaceProvider = (({children}) => {
   const [ marketplace, setMarketplace ] = useState({})
   const [ address, setAddress ] = useState("")
+  const [marketplaceData, setMarketplaceData] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
+  let lastProcessedId = 0
 
+  const fetchContract = (signerOrProvider) => new ethers.Contract(marketplaceAddress, MarketplaceABI.abi, signerOrProvider)
 
   // Initialize Contract
   const initializeContract = async () => {
@@ -26,12 +34,12 @@ export const MarketplaceProvider = (({children}) => {
         "method": "eth_chainId",
         "params": [],
         "id": 0
-      });
+      })
       if (chainId === "0x118") {
         try {
           const provider = new ethers.providers.Web3Provider(window.ethereum)
           const signer = provider.getSigner()
-          const marketplace =  new ethers.Contract(marketplaceAddress, MarketplaceABI.abi, signer)
+          const marketplace =  fetchContract(signer)
           setMarketplace(marketplace)
         } catch (error) {
           console.log(error)
@@ -42,50 +50,19 @@ export const MarketplaceProvider = (({children}) => {
     }
   }
 
-  // Connect and Initialize Wallet
-  const initializeWallet = async () => {
-    console.log("handler triggered")
-    if (window.ethereum) {
-      const chainId = await window.ethereum.request({ 
-        "jsonrpc": "2.0",
-        "method": "eth_chainId",
-        "params": [],
-        "id": 0
-      });
-
-      if (chainId === "0x118") {
-        try{
-          console.log(true)
-          const account = await window.ethereum.request({ 
-            method: 'eth_requestAccounts'
-          });
-          console.log(chainId)
-        }catch (error) {
-            console.log(error)
-            console.log('false')
-        }
-
-      }
-    } else {
-        alert("Please install MetaMask")
-    }
-    
-  }
-
   const connectWallet = async () => {
-    
     if (window.ethereum) {
       try {
         await window.ethereum.request({
           method: 'wallet_switchEthereumChain',
           params: [{ chainId: '0x118' }], 
-        });
+        })
 
         const account = await window.ethereum.request({ 
           method: 'eth_requestAccounts'
-        });
+        })
         setAddress(account[0])
-        console.log(account[0])
+       // console.log(account[0])
 
       } catch (error) {
         if (error.code === 4902) {
@@ -105,9 +82,9 @@ export const MarketplaceProvider = (({children}) => {
                   blockExplorerUrls: ["https://explorer.zksync.io/"]
                 },
               ],
-            });
+            })
           } catch (error) {
-            console.log(error);
+            console.log(error)
           }
         }
 
@@ -118,7 +95,7 @@ export const MarketplaceProvider = (({children}) => {
   
 
   window.ethereum.on('chainChanged', (chainId) => {
-    window.location.reload();
+    window.location.reload()
   })
 
   window.ethereum.on('accountsChanged', async function (account) {
@@ -139,7 +116,7 @@ export const MarketplaceProvider = (({children}) => {
       const listingPrice = ethers.utils.parseUnits(_price.toString(), 'ether')
       await marketplace.listItem(nftId, listingPrice)
 
-      await axios.post(`http://api.nftpinas.io/v1/nfts/${_authorId}/${_tokenId}`, {
+      await axios.patch(`http://api.nftpinas.io/v1/nfts/${_authorId}/${_tokenId}`, {
         isListed: true,
         price: _price
       })
@@ -151,7 +128,7 @@ export const MarketplaceProvider = (({children}) => {
   }
 
   // Mint Item from the Marketplace
-  const mintNFT = async (image, name, description, trait_type, _alue) => {
+  const mintNFT = async (image, name, description, trait_type, value) => {
     if ( !image || !name || !description ) return alert("Please fill out the missing field.")
 
     try {
@@ -162,10 +139,98 @@ export const MarketplaceProvider = (({children}) => {
     } catch (error) {
         console.log("Error Minting Item", error)
     }
-}
+  }
 
+  const buyNFT = async (_itemId, _price) => {
+    try {
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      const signer = provider.getSigner()
+      const contract = fetchContract(signer)
+      const price = ethers.utils.parseEther(_price.toString(), "ether")
+  
+  
+      // Call the buyItem function on the contract
+      const transaction = await contract.buyItem(_itemId, { value: price })
+  
+      // Wait for the transaction to be mined
+      await transaction.wait()
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const fetchMarketItems = async () => {
+    try {
+      const provider = new Provider("https://zksync2-testnet.zksync.dev")
+      const contract = fetchContract(provider)
+      const data = await contract.fetchMarketItems()
+      const items = await Promise.all(
+        data.map(
+          async ({itemId, tokenId, seller, owner, price: unformattedPrice}) => {
+            const price = ethers.utils.formatUnits(unformattedPrice.toString(), 'ether')
+
+            const response = await axios.patch(`http://api.nftpinas.io/v1/nfts/${seller}/${tokenId}`, {
+              isListed: true,
+              price: price
+            })
+          } 
+
+        )
+      )
+      } catch (err) {
+        console.log(err)
+    }
+  }
+
+  const convertString = ( convert, contract ) => async (_tokenId) => {
+    if(convert === 'owner'){
+      const owner = await contract.ownerOf(_tokenId)
+      return owner.toLowerCase()
+    }
+  }
+
+  const fetchAllNfts = async () => {
+    const provider = new Provider("https://zksync2-testnet.zksync.dev")
+    const contract =  new ethers.Contract(marketplaceAddress, MarketplaceABI.abi, provider)
+    const tokenCount = await contract.tokenIds()
+    const tokenId = tokenCount.toNumber()
+    //check if there is new items
+    if (lastProcessedId < tokenId) {
+      const promises = []
+      //fetch new item only
+      for (let i = lastProcessedId+1; i <= tokenId; i++) {
+        const owner = await convertString('owner', contract)(i)
+        const uri = await contract.tokenURI(i)
+        promises.push(Promise.all([owner, i, uri, axios.get(uri)]))
+      }
+      const responses = await Promise.all(promises)
+      const postData = responses.map(([owner, i, uri, {data}]) => {
+        return { 
+          owner_address: owner, 
+          tokenId: i,
+          tokenUri: uri,
+          isListed: false,
+          contract_address: contract.address.toLowerCase(),
+          metadata: {
+            image: data.image, 
+            name: data.name, 
+            description: data.description
+          }
+        }
+      })
+      
+      // POST the NFT data to the API
+      const postPromises = postData.map((data) => axios.post("http://api.nftpinas.io/v1/nfts/", data))
+      await Promise.all(postPromises)
+      console.log(postPromises)
+      lastProcessedId = tokenId
+     }
+  }
+
+
+  
   return (
-    <MarketplaceContext.Provider value={{initializeContract, listItem, mintNFT, connectWallet, address}}>
+    <MarketplaceContext.Provider value={{initializeContract, listItem, mintNFT, buyNFT, connectWallet, fetchMarketItems, fetchAllNfts, address, marketplaceData, setMarketplaceData}}>
       {children}
     </MarketplaceContext.Provider>
   )
