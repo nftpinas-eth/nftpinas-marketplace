baa28ee8351740d0ad98c8d560c7d21fe264329a
0xSimbaCrypto <0xSimbaCrypto@proton.me>
2023-02-19 15:52:44 +0800

    Add NFTCard Components

--- components/NFTCard/nftcard.jsx
+++ components/NFTCard/nftcard.jsx
@@ -1,37 +1,65 @@
 import React from 'react'
 import Image from 'next/image'
+import { FaEthereum } from "react-icons/fa"
+import { CgProfile } from "react-icons/cg"
+import { useRouter } from 'next/router';
 
-const style = {
-    nftContainer: `overflow-hidden border shadow rounded-xl`,
-    title: `font-mono font-black text-4xl text-slate-900 `,
-    subTitle: `font-mono font-black text-lg text-slate-900`,
-    description: `font-mono font-semibold text-sm text-slate-700`
+const NftCard = ({ src, alt, title, description, price, isListed, marketId, tokenId, creator, onClick }) => {
+  const router = useRouter()
+  const goToAuthorPage = (address) => {
+    router.push(`/mynfts?address=${address}`)
   }
-
-  const NftCard = ({ image, name, description, price, onClick }) => {
-    return (
-      <div onClick={onClick} className="nft-card">
-            <div  className={style.nftContainer}>
-                <Image
-                    src={image}
-                    alt="NFT Image Here"
-                    width={500}
-                    height={500}
-                />
-                <div className="px-4">
-                    <p style={{ height: '32px'}} className={style.subTitle}>
-                    {name}
-                    </p>
-                    <div style={{ height: '50px', overflow: 'hidden'}}>
-                    <p className={style.description}>{description}</p>
-                    </div>
-                    <div style={{ height: '50px', overflow: 'hidden'}}>
-                    <p className={style.subTitle}>{price} ETH</p>
-                    </div>
-                </div>
-            </div>
-      </div>
-    );
-  };
   
-  export default NftCard;
+  return (
+    <div  className='flex flex-col w-[300px] h-[470px] px-[14px] mx-auto bg-[#2A292B] drop-shadow-md rounded-xl '>
+    <div className='relative w-[270px] h-[250px] my-[14px]'>
+        <Image onClick={onClick}
+            src={src}
+            alt={alt}
+            layout='fill'
+            objectFit='fit'
+            className='cursor-pointer rounded-xl'
+        />
+        {!isListed ? (
+          <div className='absolute flex m-[4px] flex-col'>
+            <div className='bg-black bg-slate-900 font-mono text-white text-sm font-semibold px-[0.5rem] rounded-lg w-max mb-[4px]'>Token #{tokenId}</div>
+          </div>
+        ) : 
+          <div className='absolute flex m-[4px] flex-col'>
+            <div className='bg-black bg-slate-900 font-mono text-white text-sm font-semibold px-[0.5rem] rounded-lg w-max mb-[4px]'>Token #{tokenId}</div>
+            <div className='bg-red-800 font-mono text-white text-sm font-semibold px-[0.5rem] rounded-lg w-max'>Market #{marketId}</div>
+          </div>
+        }
+
+    </div>
+
+    <div className='max-woverflow-y-hidden text-overflow-ellipsis line-clamp-2'>
+      <p className='text-white font-bold text-[20px]'>{title}</p>
+      <p className='overflow-y-hidden text-gray-400 text-overflow-ellipsis line-clamp-2'>lorem</p>
+    </div>
+
+    <div className='flex mt-[20px]'>
+        {!isListed ? (
+          <div className='flex items-center'>
+            <FaEthereum className='w-[17px] h-[17px] text-[#DD9F00]'/>
+            <p className='font-bold text-[#DD9F00]'>Not Listed</p>
+          </div>
+        ) : 
+        <div className='flex items-center'>
+          <FaEthereum className='w-[17px] h-[17px] text-[#DD9F00]'/>
+          <p className='font-bold text-[#DD9F00]'>{price}</p>
+        </div>
+
+        }
+
+    </div>
+    <hr className='my-[10px] bg-zinc-400' />
+    <div className='flex items-center justify-center'>
+          <CgProfile className='w-[40px] h-[40px] text-white'/>
+          <p className='font-bold text-gray-400 mx-[10px]'>Creation of <span onClick={()=>goToAuthorPage(creator)} className='text-white cursor-pointer'>{creator.slice(0,4) + "..."  + creator.slice(39,42)}</span></p>
+    </div>
+    </div>
+  )
+  }
+  
+  export default NftCard
