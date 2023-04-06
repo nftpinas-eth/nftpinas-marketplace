c83a776d46d548f840696dee12d9c1059a79943f
0xSimbaCrypto <0xSimbaCrypto@proton.me>
2023-04-06 14:03:51 +0800

    Clean Index

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,6 @@
 //import type { NextPage } from 'next'
-import Header from '../components/Header'
 import Hero from '../components/Hero'
 import About from '../components/About'
-import Footer from '../components/Footer'
-import Head from 'next/head'
-import Image from 'next/image'
-import { useEffect } from 'react'
 
 const Home = () => {
   return (
