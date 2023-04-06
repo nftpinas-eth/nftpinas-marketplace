64dcba1cb8ae7dde89d956b9754df79abca62d6b
0xSimbaCrypto <0xSimbaCrypto@proton.me>
2023-04-06 14:03:22 +0800

    Update Context on My App

--- pages/_app.js
+++ pages/_app.js
@@ -5,63 +5,34 @@ import Header from "../components/Header"
 import Footer from "../components/Footer"
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
-import { MarketplaceProvider } from '../context/MarketplaceContext'
-
+import { WalletProvider } from "../context/WalletContext";
+import { ContractContextProvider } from "../context/ContractContext";
+import { MarketplaceActionsProvider } from "../context/MarketplaceActionsContext";
+import { DataFetchProvider } from "../context/DataFetchContext";
 
 function MyApp({ Component, pageProps }) {
   const [queryClient] = React.useState(() => new QueryClient())
 
-  useEffect(() => {
-    const checkProvider = async () => {
-      if (window.ethereum) {
-        try {
-          await window.ethereum.request({
-            method: 'wallet_switchEthereumChain',
-            params: [{ chainId: '0x118' }], 
-          });
-        } catch (error) {
-          if (error.code === 4902) {
-            try {
-              await window.ethereum.request({
-                method: 'wallet_addEthereumChain',
-                params: [
-                  {   
-                    chainId: '0x118',
-                    chainName: 'zkSync alpha testnet',
-                    rpcUrls: ['https://zksync2-testnet.zksync.dev'],
-                    nativeCurrency: {
-                        name: "zkSync ETH",
-                        symbol: "ETH",
-                        decimals: 18
-                    },
-                    blockExplorerUrls: ["https://explorer.zksync.io/"]
-                  },
-                ],
-              });
-            } catch (error) {
-              console.log(error);
-            }
-          }
-        }
-      }
-    }
-  }, [])
-
-
   return (
     <QueryClientProvider client={queryClient}>
-      <MarketplaceProvider>
-      <div className="flex flex-col min-h-screen bg-[#1F1D1B]">
-        <Header />
-        <main className="flex-grow container mx-auto px-4 py-8">
-        <Component {...pageProps} />
-        </main>
-        <Footer />
-        <ReactQueryDevtools initialIsOpen={false} />
-        </div>
-      </MarketplaceProvider>
-   </QueryClientProvider>
-  )
+      <WalletProvider>
+        <ContractContextProvider>
+          <MarketplaceActionsProvider>
+            <DataFetchProvider>
+              <div className="flex flex-col min-h-screen bg-[#1F1D1B]">
+                <Header />
+                  <main className="flex-grow container mx-auto px-4 py-8">
+                    <Component {...pageProps} />
+                  </main>
+                <Footer />
+                <ReactQueryDevtools initialIsOpen={false} />
+              </div>
+            </DataFetchProvider>
+          </MarketplaceActionsProvider>
+        </ContractContextProvider>
+      </WalletProvider>
+    </QueryClientProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
